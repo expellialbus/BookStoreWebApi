b6db0efed9fbb971109bebd0a24f31c47b3c6439
Recep YILDIRIM <0.recepyildirim@gmail.com>
2022-01-23 00:11:47 +0300

    Database context interface has been created.

---
+++ WebApi/DbOperations/IBookStoreDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using WebApi.Entities;
+
+namespace WebApi.DbOperations
+{
+    public interface IBookStoreDbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public int SaveChanges();
+    }
+}
