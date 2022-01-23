09c7bf2847ce657606956a65374d13affcd04e41
Recep YILDIRIM <0.recepyildirim@gmail.com>
2022-01-23 18:00:52 +0300

    Mapping rules for BookViewModel class have been added.

--- WebApi/Common/MappingProfile.cs
+++ WebApi/Common/MappingProfile.cs
@@ -1,4 +1,6 @@
 using AutoMapper;
+using WebApi.Applications.BookOperations.Queries.GetBooks;
+using WebApi.Entities;
 
 namespace WebApi.Common
 {
@@ -6,7 +8,12 @@ namespace WebApi.Common
     {
         public MappingProfile()
         {
-            
+            CreateMap<Book, BookViewModel>().ForMember(model => model.Genre,
+                options => options.MapFrom(book => book.Genre.Name));
+            CreateMap<Book, BookViewModel>().ForMember(model => model.Author,
+                options => options.MapFrom(book => book.Author.Name));
+            CreateMap<Book, BookViewModel>().ForMember(model => model.PublishDate,
+                options => options.MapFrom(book => book.PublishDate.ToString("dd/MM/yyyy")));
         }
     }
 }
