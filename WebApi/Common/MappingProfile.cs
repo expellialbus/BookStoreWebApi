2cb5927b8dc7aa5241875b16722a2e95d7bfc155
Recep YILDIRIM <0.recepyildirim@gmail.com>
2022-01-23 17:40:31 +0300

    Profile file for auto mapping has been created.

---
+++ WebApi/Common/MappingProfile.cs
@@ -0,0 +1,12 @@
+using AutoMapper;
+
+namespace WebApi.Common
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            
+        }
+    }
+}
--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -31,6 +32,7 @@ namespace WebApi
             services.AddDbContext<BookStoreDbContext>(options =>
                 options.UseInMemoryDatabase(databaseName: "BookStoreDB"));
             services.AddScoped<IBookStoreDbContext>(provider => provider.GetService<BookStoreDbContext>());
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
