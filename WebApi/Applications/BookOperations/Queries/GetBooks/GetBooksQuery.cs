0f2cdfea09f9bb4cf5b855c966d067546ae3ae42
Recep YILDIRIM <0.recepyildirim@gmail.com>
2022-01-23 18:00:15 +0300

    Service class to get all books from database has been created.

---
+++ WebApi/Applications/BookOperations/Queries/GetBooks/GetBooksQuery.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using WebApi.DbOperations;
+
+namespace WebApi.Applications.BookOperations.Queries.GetBooks
+{
+    public class GetBooksQuery
+    {
+        private readonly IBookStoreDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetBooksQuery(IBookStoreDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public List<BookViewModel> Handle()
+        {
+            var books = _context.Books
+                .Include(book => book.Author)
+                .Include(book => book.Genre)
+                .OrderBy(book => book.Id).ToList();
+
+            return _mapper.Map<List<BookViewModel>>(books);
+        }
+    }
+
+    public class BookViewModel
+    {
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public string Author { get; set; }
+        public int PageCount { get; set; }
+        public string PublishDate { get; set; }
+    }
+}
