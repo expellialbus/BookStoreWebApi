1d6d317bc2fcdfa57bd16193618443f43d3f970b
Recep YILDIRIM <0.recepyildirim@gmail.com>
2022-01-24 22:36:38 +0300

    Missing surname information has been added.

--- WebApi/Common/MappingProfile.cs
+++ WebApi/Common/MappingProfile.cs
@@ -8,13 +8,19 @@ namespace WebApi.Common
     {
         public MappingProfile()
         {
+            BookMaps();
+        }
+
+        // Maps Book objects to BookViewModel according to specified rules
+        private void BookMaps()
+        {
             // Maps Genre.Name to Genre property of BookViewModel
             CreateMap<Book, BookViewModel>().ForMember(model => model.Genre,
                 options => options.MapFrom(book => book.Genre.Name));
 
-            // Maps Author.Name to Author property of BookViewModel
+            // Maps Author.Name and Author.Surname to Author property of BookViewModel
             CreateMap<Book, BookViewModel>().ForMember(model => model.Author,
-                options => options.MapFrom(book => book.Author.Name));
+                options => options.MapFrom(book => book.Author.Name + " " + book.Author.Surname));
 
             // Maps Book.PublishDate to PublishDate property of BookViewModel as string
             CreateMap<Book, BookViewModel>().ForMember(model => model.PublishDate,
