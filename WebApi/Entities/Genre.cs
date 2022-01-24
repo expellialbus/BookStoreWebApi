3603bbd69212a52b5b0049b2248f138a80358e41
Recep YILDIRIM <0.recepyildirim@gmail.com>
2022-01-24 22:34:23 +0300

    Default value has been added for IsActive property.

--- WebApi/Entities/Genre.cs
+++ WebApi/Entities/Genre.cs
@@ -8,6 +8,6 @@ namespace WebApi.Entities
         public int Id { get; set; }
         
         public string Name { get; set; }
-        public bool IsActive { get; set; }
+        public bool IsActive { get; set; } = true;
     }
 }
