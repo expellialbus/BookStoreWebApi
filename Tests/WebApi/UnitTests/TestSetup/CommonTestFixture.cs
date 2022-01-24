1e54884fd2ec89ea4b30987ce284057c57f3b874
Recep YILDIRIM <0.recepyildirim@gmail.com>
2022-01-24 22:57:25 +0300

    Test project's setup has been completed.

---
+++ Tests/WebApi/UnitTests/TestSetup/CommonTestFixture.cs
@@ -0,0 +1,39 @@
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using WebApi.Common;
+using WebApi.DbOperations;
+
+namespace UnitTests.TestSetup
+{
+    public class CommonTestFixture
+    {
+        public IBookStoreDbContext Context { get; set; }
+        public IMapper Mapper { get; set; }
+
+        public CommonTestFixture()
+        {
+            
+            // Builds options for BookStoreDbContext 
+            var options = new DbContextOptionsBuilder<BookStoreDbContext>()
+                .UseInMemoryDatabase(databaseName: "BookStoreTestDB").Options;
+            
+            // Creates context
+            var context = new BookStoreDbContext(options);
+            
+            // Ensures database created
+            context.Database.EnsureCreated();
+            
+            // Add test data to the database
+            context.AddBooks();
+            context.AddGenres();
+            context.AddAuthors();
+
+            context.SaveChanges();
+
+            Context = context;
+            
+            // Creates a mapper object with the configurations of MappingProfile class
+            Mapper = new MapperConfiguration(expression => expression.AddProfile<MappingProfile>()).CreateMapper();
+        }
+    }
+}
