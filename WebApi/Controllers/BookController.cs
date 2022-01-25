1dafad78cc8bf65a0f44e1302a32a235cb824a62
Recep <0.recepyildirim@gmail.com>
2022-01-26 02:31:58 +0300

    GetById endpoint for Book entity has been added.

--- WebApi/Controllers/BookController.cs
+++ WebApi/Controllers/BookController.cs
@@ -1,5 +1,6 @@
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using WebApi.Applications.BookOperations.Queries.GetBookDetail;
 using WebApi.Applications.BookOperations.Queries.GetBooks;
 using WebApi.DbOperations;
 
@@ -24,6 +25,19 @@ namespace WebApi.Controllers
             // Creates an instance of GetBooksQuery class and returns all books to the client
             // Returns 200 OK with list of books if there is any data in the books table
             GetBooksQuery query = new GetBooksQuery(_context, _mapper);
+
+            var result = query.Handle();
+
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            // Creates and instance of GetBookDetailQuery and returns book specified by id 
+            // Return 200 OK if there is the specified book in the database
+            GetBookDetailQuery query = new GetBookDetailQuery(_context, _mapper);
+            query.BookId = id;
 
             var result = query.Handle();
 
