330a35ba6f1919d0af192be53d9b132dea14766c
Recep <0.recepyildirim@gmail.com>
2022-01-28 00:59:04 +0300

    Validator for Book's Delete endpoint has been tested.

---
+++ Tests/WebApi/UnitTests/Applications/BookOperations/Commands/DeleteBook/DeleteBookCommandValidatorTests.cs
@@ -0,0 +1,40 @@
+using FluentAssertions;
+using FluentValidation;
+using WebApi.Applications.BookOperations.Commands.DeleteBook;
+using Xunit;
+
+namespace UnitTests.Applications.BookOperations.Commands.DeleteBook
+{
+    public class DeleteBookCommandValidatorTests
+    {
+        [Fact]
+        public void WhenValidIdIsGiven_Validator_ShouldNotReturnError()
+        {
+            // Since this function tests the validator 
+            // the command object's property won't be used
+            DeleteBookCommand command = new DeleteBookCommand(null);
+            DeleteBookCommandValidator validator = new DeleteBookCommandValidator();
+
+            command.BookId = 5; // it does not matter the book with provided is in database
+
+            var result = validator.Validate(command);
+
+            result.Errors.Count.Should().Be(0);
+        }
+
+        [Fact]
+        public void WhenInvalidIdIsGiven_Validator_ShouldReturnError()
+        {
+            // Since this function tests the validator 
+            // the command object's property won't be used
+            DeleteBookCommand command = new DeleteBookCommand(null);
+            DeleteBookCommandValidator validator = new DeleteBookCommandValidator();
+
+            command.BookId = 0; // it does not matter the book with provided is in database
+            
+            var result = validator.Validate(command);
+
+            result.Errors.Count.Should().BeGreaterThan(0);
+        }
+    }
+}
