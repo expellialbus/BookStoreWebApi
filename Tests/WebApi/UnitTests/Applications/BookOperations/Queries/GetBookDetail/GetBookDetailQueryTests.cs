f2300a02b047c030374bc87dbc2934a98b971e63
Recep <0.recepyildirim@gmail.com>
2022-01-27 21:22:17 +0300

    Service for Book's GetById endpoint has been tested.

---
+++ Tests/WebApi/UnitTests/Applications/BookOperations/Queries/GetBookDetail/GetBookDetailQueryTests.cs
@@ -0,0 +1,45 @@
+using System;
+using AutoMapper;
+using FluentAssertions;
+using UnitTests.TestSetup;
+using WebApi.Applications.BookOperations.Queries.GetBookDetail;
+using WebApi.DbOperations;
+using Xunit;
+
+namespace UnitTests.Applications.BookOperations.Queries.GetBookDetail
+{
+    public class GetBookDetailQueryTests : IClassFixture<CommonTestFixture>
+    {
+        private readonly IBookStoreDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetBookDetailQueryTests(CommonTestFixture fixture)
+        {
+            _context = fixture.Context;
+            _mapper = fixture.Mapper;
+        }
+
+        [Fact]
+        public void WhenValidIdIsGiven_Book_ShouldBeReturned()
+        {
+            GetBookDetailQuery query = new GetBookDetailQuery(_context, _mapper);
+            query.BookId = 1;
+            
+            FluentActions
+                .Invoking(() => query.Handle())
+                .Should().NotBeNull();
+        }
+
+        [Fact]
+        public void WhenInvalidIdIsGiven_InvalidOperationException_ShouldBeThrown()
+        {
+            GetBookDetailQuery query = new GetBookDetailQuery(_context, _mapper);
+            query.BookId = 5;
+
+            FluentActions
+                .Invoking(() => query.Handle())
+                .Should().Throw<InvalidOperationException>()
+                .WithMessage("No such book.!");
+        }
+    }
+}
