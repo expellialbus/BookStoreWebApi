044782e9e6520ed8a6495654bbb2264b997c3bbd
Recep <0.recepyildirim@gmail.com>
2022-01-29 22:32:02 +0300

    GetById endpoint for Author has been created.

--- WebApi/Controllers/AuthorController.cs
+++ WebApi/Controllers/AuthorController.cs
@@ -1,5 +1,6 @@
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using WebApi.Applications.AuthorOperations.Queries.GetAuthorDetail;
 using WebApi.Applications.AuthorOperations.Queries.GetAuthors;
 using WebApi.DbOperations;
 
@@ -25,6 +26,20 @@ namespace WebApi.Controllers
             // Returns 200 OK if there is any data in the authors table
             GetAuthorsQuery query = new GetAuthorsQuery(_context, _mapper);
 
+            var result = query.Handle();
+
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            // Creates an instance of GetAuthorDetailQuery class and returns author specified by id
+            // Returns 200 OK if there is the specified author in the authors table
+            GetAuthorDetailQuery query = new GetAuthorDetailQuery(_context, _mapper);
+
+            query.AuthorId = id;
+            
             var result = query.Handle();
 
             return Ok(result);
