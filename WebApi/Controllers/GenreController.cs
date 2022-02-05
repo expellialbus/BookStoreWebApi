dfb15f4ce856153599e1e97958f7fc22db2c980e
Recep <0.recepyildirim@gmail.com>
2022-02-05 20:44:06 +0300

    Validator for Genre's Post endpoint has been created.

--- WebApi/Controllers/GenreController.cs
+++ WebApi/Controllers/GenreController.cs
@@ -60,9 +60,15 @@ namespace WebApi.Controllers
             // Creates and instance of CreateGenreCommand and adds new genre to the database
             // Returns 200 OK if the genre is successfully added to the database
             CreateGenreCommand command = new CreateGenreCommand(_context, _mapper);
-
+            CreateGenreCommandValidator validator = new CreateGenreCommandValidator();
+            
             command.Model = model;
             
+            // Tries to validate CreateGenreCommand class properties
+            // according to rules provided in CreateGenreCommandValidator class constructor
+            // If properties are not consistent with the rules 
+            // an error will be thrown
+            validator.ValidateAndThrow(command);
             command.Handle();
 
             return Ok();
