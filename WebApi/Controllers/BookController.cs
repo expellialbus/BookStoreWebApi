3bebc450dfc9ce5b91eea81594781999d9a53734
Recep <0.recepyildirim@gmail.com>
2022-02-09 20:25:12 +0300

    Authorization has been added.

--- WebApi/Controllers/BookController.cs
+++ WebApi/Controllers/BookController.cs
@@ -1,5 +1,6 @@
 using AutoMapper;
 using FluentValidation;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using WebApi.Applications.BookOperations.Commands.CreateBook;
 using WebApi.Applications.BookOperations.Commands.DeleteBook;
@@ -10,6 +11,7 @@ using WebApi.DbOperations;
 
 namespace WebApi.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("[controller]s")]
     public class BookController : ControllerBase
